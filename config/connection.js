2ac5e7da943f8aec27af257f4c0b2c4367c3a8c4
Mandolin Daze <renormalize@gmail.com>
2021-08-23 05:29:29 -0700

    modified connections for heroku?

--- config/connection.js
+++ config/connection.js
@@ -3,7 +3,13 @@ require('dotenv').config();
 const Sequelize = require('sequelize');
 
 const sequelize = process.env.DATABASE_URL
-  ? new Sequelize(process.env.DATABASE_URL)
+  ? new Sequelize(process.env.DATABASE_URL, {
+    
+    dialect: 'mysql',
+    dialectOptions: {
+      decimalNumbers: true,
+    }
+  })
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
       host: 'localhost',
       dialect: 'mysql',
